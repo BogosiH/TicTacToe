138f4aeb5aa52d7b8f31e03e045ce31de002d25e
Harry Modise <harrymodise22@gmail.com>
2022-04-09 16:06:36 +0200

    TicTacToe project files

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+
+/**
+ * 
+ * @author Bogosi Modise
+ * This is the main class to instantiate the tictactoe class
+ *
+ */
+public class Main {
+
+	public static void main(String[] args) {
+		
+		TicTacToe tictac = new TicTacToe();
+
+	}
+
+}
